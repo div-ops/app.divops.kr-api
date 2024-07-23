9136e70892e72d2f1d7b90f14088ee3f0edce879
divopsor <divopsor@gmail.com>
2024-07-23 20:59:37 +0900

    ignore [skip vercel]

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ module.exports = (phase, { defaultConfig }) => {
     // ...defaultConfig,
     reactStrictMode: true,
     swcMinify: true,
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
   };
 
   return nextConfig;
